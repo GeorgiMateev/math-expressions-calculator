fad4417889b2668cb61c1f6f202eeb71771ccc12
gmmateev@gmail.com <gmmateev@gmail.com@03862886-fbfb-45fd-4713-66270d125f62>
2012-07-04 21:41:43 +0000

    Upload solution files

---
+++ trunk/TestCalculateExpressions/TestFunctionLn.cs
@@ -0,0 +1,33 @@
+﻿using CalculateExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace TestCalculateExpressions
+{
+    /// <summary>
+    /// <copyright>
+    ///     Georgi Mateev, 2012 http://www.georgi-mateev.com
+    /// </copyright>
+    /// </summary>
+    public partial class TestCalculateExpressionInRPN
+    {
+
+        /// <summary>
+        ///A test for FunctionLn
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("CalculateExpressions.exe")]
+        public void TestFunctionLnInMiddle()
+        {
+            List<object> list = new List<object>() { 2.0, 4.0, "sqrt", "-",1.0, "ln" }; // TODO: Initialize to an appropriate value
+            int index = 5; // TODO: Initialize to an appropriate value
+            int indexExpected = 4; // TODO: Initialize to an appropriate value
+            double expected = 0; // TODO: Initialize to an appropriate value
+            double actual;
+            actual = CalculateExpressions_Accessor.FunctionLn(list, ref index);
+            Assert.AreEqual(indexExpected, index);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
